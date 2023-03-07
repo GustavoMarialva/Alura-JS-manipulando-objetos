9170ad14ecde07d7779ebf05c51dc3bfb8b8720b
Gustavo Marialva <gmarialva@gmail.com>
2023-03-07 13:47:01 -0300

    cria function para mostraItem de compra adicionado

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ let listaItem = [];
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
 
 form.addEventListener("submit", function (evento) {
   evento.preventDefault();
   salvarItem();
+  mostrarItem();
 });
 
 function salvarItem() {
@@ -23,4 +25,20 @@ function salvarItem() {
   }
 
   console.log(listaItem);
+}
+
+function mostrarItem() {
+  ulItens.innerHTML = "";
+  listaItem.forEach((elemento, index) => {
+    ulItens.innerHTML += `
+    <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>`;
+  });
 }
